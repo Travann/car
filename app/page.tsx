5b19e208e81425dc58fd9e5c36b8aa1189188e50
Kun Oracle <ploykampochii@gmail.com>
2023-12-01 19:33:06 +0700

    w

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
-import { Hero, NavBar } from "@/components";
+import { Footer, Hero, NavBar } from "@/components";
 
 export default function Home() {
   return (
-    <main className="overflow-hidden">
-      <div className="gradient" />
-      <NavBar />
-      <Hero />
-      <div className={"mt-12 padding-x padding-y max-width"} id={"discover"}>
-        <div className={"home__text-container"}>
-          <h1 className={"text-4xl font-extrabold"}>Car Catalogue</h1>
+    <>
+      <main className="overflow-hidden">
+        <NavBar />
+        <div className="gradient" />
+        <Hero />
+        <div className={"mt-12 padding-x padding-y max-width"} id={"discover"}>
+          <div className={"home__text-container"}>
+            <h1 className={"text-4xl font-extrabold"}>Car Catalogue</h1>
+          </div>
         </div>
-      </div>
-    </main>
+        <Footer />
+      </main>
+    </>
   );
 }
